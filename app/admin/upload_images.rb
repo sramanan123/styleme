255f004c390ae008eeaadb4123d1495c23dd04cc
anonymous anonymous <shalini.ramanan@kellogg.northwestern.edu>
2021-12-08 20:22:56 +0000

    Whitelist columns in UploadImage for ActiveAdmin

--- app/admin/upload_images.rb
+++ app/admin/upload_images.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register UploadImage do
 
+ permit_params :image, :topstyle, :topcolor, :bottomstyle, :bottomcolor, :shoestyle, :shoecolor, :vibe
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
