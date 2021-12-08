505094bfc9f40d04b52e99fc92db17b1519b0621
anonymous anonymous <shalini.ramanan@kellogg.northwestern.edu>
2021-12-08 20:22:45 +0000

    Direct association between users and bookmarks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :bookmarks,
+             :dependent => :destroy
+
   has_many   :image_comments,
              :foreign_key => "commentor_id",
              :dependent => :destroy
